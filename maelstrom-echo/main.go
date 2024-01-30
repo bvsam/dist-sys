6b016c53620c43d690eaccd0a9bb252b236d4abf
Benjamin Sam <79420331+bvsam@users.noreply.github.com>
2024-01-30 02:19:25 -0500

    Formatting files.

--- maelstrom-echo/main.go
+++ maelstrom-echo/main.go
@@ -7,7 +7,7 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-func main(){
+func main() {
 	n := maelstrom.NewNode()
 	n.Handle("echo", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
@@ -15,15 +15,15 @@ func main(){
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-	
+
 		// Update the message type to return back.
 		body["type"] = "echo_ok"
-	
+
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, body)
 	})
-	
+
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
-	}	
+	}
 }
